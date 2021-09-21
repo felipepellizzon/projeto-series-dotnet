5468ffadbbeb82feb0fe390a27dfe2375cc40109
felipepellizzon <fapellizzon@gmail.com>
2021-09-21 14:00:28 -0300

    primeiro commit e inicio metodo listar

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Alcantara.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id{ get; protected set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Alcantara.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        //InserirSeries();
+                        break;
+                    case "3":
+                        //AtualizarSeries();
+                        break;
+                    case "4":
+                       // ExcluirSeries();
+                        break;
+                    case "5":
+                      //  VisualizarSeries();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar esse programa!");
+            Console.ReadLine();
+            
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Alcantara Series está aqui !!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar Séries");
+            Console.WriteLine("2- Inserir nova Série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
